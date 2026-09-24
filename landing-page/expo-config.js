9bb265936d0a315ffce4e1f985a2d5effacc062c
raghavrecharge <130034271+raghavrecharge@users.noreply.github.com>
2026-09-24 13:05:31 +0530

    Use Global Uni Expo name in configuration

--- landing-page/expo-config.js
+++ landing-page/expo-config.js
@@ -1,2 +1,2 @@
-/* Form delivery endpoint for Global UniExpo Indore. */
+/* Form delivery endpoint for Global Uni Expo Indore. */
 window.EXPO_CONFIG = { appsScriptUrl: "https://script.google.com/macros/s/AKfycbyHn9J57MrkRPLzUF4WCPh8eogwPP4RrU6-tCo4z1p6LTrfLZuerDgW12x2TGeF8I7F/exec", googleMapsApiKey: "", geicSearchQuery: "GEIC Indore study abroad consultants" };
